8564a1bd68fc9697dc63fc124e044643fd5b98f2
uwebury <142432719+uwebury@users.noreply.github.com>
2023-12-07 11:21:03 +0100

    Added default values

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import "./styles.css";
 import useLocalStorageState from "use-local-storage-state";
 
 export default function App() {
-  const [note, setNote] = useLocalStorageState("");
-  const [font, setFont] = useLocalStorageState("system-ui");
+  const [note, setNote] = useLocalStorageState("note", { defaultValue: "" });
+  const [font, setFont] = useLocalStorageState("font", {
+    defaultValue: "system-ui",
+  });
 
   function handleNoteChange(newNote) {
     setNote(newNote);
